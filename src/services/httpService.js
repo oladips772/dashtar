160984662d4e11962cf96a1e3036c1d9bb49e87d
oladips772 <oladips200@gmail.com>
2024-05-30 10:23:57 +0100

    changed http service baseurl

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_APP_API_BASE_URL}`,
+  baseURL: `https://doormut.onrender.com`,
   timeout: 50000,
   headers: {
     Accept: "application/json",
